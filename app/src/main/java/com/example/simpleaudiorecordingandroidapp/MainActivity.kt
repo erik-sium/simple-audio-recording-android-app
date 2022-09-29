c2339efbb7fb9cfac3763d9ed7f300bce5f36c4d
erik-sium <eriksingh15@gmail.com>
2022-09-29 21:41:51 +0200

    Added simple audio recording & playback functionality

--- app/src/main/java/com/example/simpleaudiorecordingandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/simpleaudiorecordingandroidapp/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.simpleaudiorecordingandroidapp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.simpleaudiorecordingandroidapp.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +14,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        if(isMicrophonePresent()) {
+            getMicrophonePermission()
+        }
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+        }
+    }
+
+    fun isMicrophonePresent(): Boolean {
+        return this.packageManager.hasSystemFeature(PackageManager.FEATURE_MICROPHONE);
+    }
+
+    fun getMicrophonePermission(): Unit {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 200)
         }
     }
 }
--- app/src/main/java/com/example/simpleaudiorecordingandroidapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/simpleaudiorecordingandroidapp/ui/main/MainFragment.kt
@@ -1,14 +1,26 @@
 package com.example.simpleaudiorecordingandroidapp.ui.main
 
+import android.content.ContextWrapper
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.simpleaudiorecordingandroidapp.R
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
 
 class MainFragment : Fragment() {
+
+    private lateinit var audioRecorder: MediaRecorder
+    private lateinit var audioPlayer: MediaPlayer
 
     companion object {
         fun newInstance() = MainFragment()
@@ -29,4 +41,79 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recordButton: Button? = getView()?.findViewById(R.id.recordButton)
+        recordButton?.setOnClickListener {
+            view -> btnRecordPressed()
+        }
+
+        val stopRecordingButton: Button? = getView()?.findViewById(R.id.stopRecordingButton)
+        stopRecordingButton?.setOnClickListener {
+            view -> btnStopRecordingPressed()
+        }
+
+        val playButton: Button? = getView()?.findViewById(R.id.playButton)
+        playButton?.setOnClickListener {
+            view -> btnPlayAudioPressed()
+        }
+    }
+
+    private fun getAudioRecordingFilePath(): String {
+
+        val contextWrapper: ContextWrapper = ContextWrapper(activity?.applicationContext)
+        val musicDirectory: File? = contextWrapper.getExternalFilesDir(Environment.DIRECTORY_MUSIC)
+
+        if(musicDirectory != null && musicDirectory.exists()) {
+            val file: File = File(musicDirectory, "testRecording.3gp")
+            return file.path
+        }
+
+        throw FileNotFoundException("cannot find Music folder")
+    }
+
+    private fun btnRecordPressed(): Unit {
+
+        try {
+            audioRecorder = MediaRecorder().apply {
+                setAudioSource(MediaRecorder.AudioSource.MIC)
+                setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+                setOutputFile(getAudioRecordingFilePath())
+                setAudioEncoder(MediaRecorder.AudioEncoder.AMR_WB)
+                setAudioEncodingBitRate(128000)
+                setAudioSamplingRate(44100)
+                prepare()
+                start()
+            }
+
+            Toast.makeText(this.context, "Recording started", Toast.LENGTH_SHORT).show()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun btnStopRecordingPressed(): Unit {
+        audioRecorder.stop()
+        audioRecorder.release()
+
+        Toast.makeText(this.context, "Recording stopped", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun btnPlayAudioPressed() : Unit {
+
+        try {
+            audioPlayer = MediaPlayer().apply {
+                setDataSource(getAudioRecordingFilePath())
+                prepare()
+                start()
+            }
+
+            Toast.makeText(this.context, "Audio recording is playing", Toast.LENGTH_SHORT).show()
+        } catch(e: Exception) {
+            e.printStackTrace()
+        }
+
+        this.context
+    }
 }
